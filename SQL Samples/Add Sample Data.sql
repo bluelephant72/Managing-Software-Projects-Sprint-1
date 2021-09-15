ed9a2cb9ce33ec464c9e20ac36a9cc3d3c9e8400
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2021-09-15 20:31:57 +1000

    First commit

---
+++ SQL Samples/Add Sample Data.sql
@@ -0,0 +1,6 @@
+-- This script will add some sample data into the database after it has been created
+
+USE php_srep_ordering;
+
+INSERT INTO employee (first_name, last_name, address, phone)
+VALUES ('Jeff', 'Smith', '28 Bourke St, Melbourne, Victoria, Australia', '0412 345 678');
