f9ab1d26dab187b013f4ff7041e17292c36e54b7
Eamonn <90015575+Eamonn8r@users.noreply.github.com>
2021-10-07 21:39:19 +1100

    Updated login page CSS

--- htdocs/login.php
+++ htdocs/login.php
@@ -10,10 +10,14 @@
 	<meta name="author" content="Anis, Eamonn, Jonno, Leonard" />
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
 
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+
 	<title>Login</title>
 </head>
 <?php
-	$page="login";  
+	$page="login";
 ?>
 <body id="loginPage">
 
@@ -27,7 +31,7 @@
 
 
 				<!--Name-->
-				<h1 id="ProjectName">People Health</br>Pharmercy</h1>
+				<h1 id="ProjectName">People Health</br>Pharmacy</h1>
 				<p>Sales Reporting and Prediction System​</p>
 
 				</br>
@@ -39,7 +43,8 @@
 				</br>
 				</br>
 				<span class="psw"><a href="#">Forgot password?</a></span>
-   			    <br>	
+   			    <br>
+						<br />
 				<!--Log in button-->
 				<input id="loginButton" type="Submit" name="Login" value="Sign In" />
 
