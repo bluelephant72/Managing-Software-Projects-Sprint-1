ad27e193cbecbd931aae5537fe8fb253d068fd68
Anis se <101278866@student.swin.edu.au>
2021-10-07 18:45:31 +1100

    home and add sales page  css styles

--- htdocs/add_sales/add_sales.php
+++ htdocs/add_sales/add_sales.php
@@ -7,45 +7,55 @@
   <meta name="description" content="Add a Sale" />
   <meta name="keywords" content="HTML, Form, tags, addsales" />
   <meta name="author" content="Anis, Eamonn, Jonno, Leonard" />
-  <link rel="stylesheet" type="text/css" href="../styles/style.css">
+  <link rel="stylesheet" type="text/css" href="../styles/styles_add_sales.css">
 </head>
 <?php
 	$page="add_sales";  
 ?>
 <title>Add Sales</title>
 
-<div class="imgcontainer">
-    <img src="../images/img_avatar1.png" alt="Avatar" class="avatar">
-</div>
 <body>
-
+<section id="add_sales_form">
+<div>
+    <form><button formaction="../login.php" id="logoutButton" type="submit">Log Out</button></form>
+  </div>
+<div class="imgcontainer">
+    <img src="../images/img_avatar1.png" id="Avatar" alt="Avatar" class="avatar">
+</div>
+   
     <div>
       <h1>People Health Pharmacy</h1>
       <h2>Add a Sale</h2>
+
     </div>
       <form  method="post" action="../scripts/php_ordering.php">
-      <label for="customerID"><b>Customer ID</b></label>
+      
+			<fieldset id="add_sale_Field">
+
+     
       <input type="number" placeholder="Enter Customer ID" name="customerID" required>
       <br>
-      <label for="productID"><b>Product ID</b></label>
+    
       <input type="number" placeholder="Enter Product ID" name="productID" required>
       <br>
-      <label for="quantity"><b>Quantity</b></label>
+       
       <input type="number" placeholder="Enter Quantity" name="quantity" required>
       <br>
-      <label for="orderDate"><b>Order Date</b></label>
+       
       <input type="datetime-local" placeholder="" name="orderDate">
       <br>
-      <label for="employeeID"><b>Employee ID</b></label>
+    
       <input type="number" placeholder="Enter Employee ID" name="employeeID" required>
       <br>
-      <button id="addSale" name="addSale" type="submit">Add Sale</button>
+      <button id="addSale" name="addSale" type="submit">Submit</button>
       <br>
+      </fieldset>
     </form>
 
 
+ 
+  
+  </section>
 
-  <div>
-    <form><button formaction="../login.php" id="logoutButton" type="submit">Log Out</button></form>
-  </div>
 </body>
+</html>
--- htdocs/scripts/home.php
+++ htdocs/scripts/home.php
@@ -7,15 +7,15 @@
   <meta name="description" content="home" />
   <meta name="keywords" content="HTML, Form, tags,payment" />
   <meta name="author" content="Anis, Eamonn, Jonno, Leonard" />
-  <link rel="stylesheet" type="text/css" href="../styles/style.css">
+  <link rel="stylesheet" type="text/css" href="../styles/style_home.css">
   
   <title>Home</title>
 </head>
 
 <body>
-    
+<section id="home_form">
     <div class="imgcontainer">
-        <img src="../images/img_avatar1.png" alt="Avatar" class="avatar">
+        <img src="../images/img_avatar1.png" id="Avatar" alt="Avatar" class="avatar">
     </div>
     <div>
         <h1>People Health Pharmacy</h1>
@@ -24,12 +24,12 @@
     <div>
         <b>Would you like to?</b>
         <br>
-        <form method="get" action="../add_sales/add_sales.php"> <button id="addSalesNavi" type="submit">Add a Sale</button></form>
-        <form method="get" action="../view_sales/display.php"> <button id="viewReportsNavi" type="submit">View Sales Reports</button>
+        <form method="get" action="add_sales.php"> <button id="addSalesNavi" type="submit">Add a Sale</button></form>
+        <form method="get" action="display.php"> <button id="viewReportsNavi" type="submit">View Sales Reports</button>
     </div>
     <br>
     <div>
-        <form><button formaction="../login.php" id="logoutButton" type="submit">Log Out</button></form>
+        <form><button formaction="login.php" id="logoutButton" type="submit">Log Out</button></form>
     </div>
-
+</section>
 </body>
